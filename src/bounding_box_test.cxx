624c7b3124ae04a2f0e7393ab5028067a595f7b4
Daniel Codex <carlo@alinoe.com>
2026-02-08 00:34:33 +0100

    bounding_box_test: add draggable rectangle corners

--- src/bounding_box_test.cxx
+++ src/bounding_box_test.cxx
@@ -3,9 +3,12 @@
 #include "cairowindow/Window.h"
 #include "cairowindow/Layer.h"
 #include "cairowindow/plot/Point.h"
+#include "cairowindow/plot/Rectangle.h"
 #include "utils/AIAlert.h"
 #include "utils/debug_ostream_operators.h"
 #include "utils/ColorPool.h"
+#include <algorithm>
+#include <functional>
 #include <iostream>
 #include <thread>
 #include "debug.h"
@@ -46,11 +49,45 @@ int main()
     CoordinateSystem<csid::painter> painter_coordinate_system(painter_transform_pixels, geometry);
 
     utils::ColorPool<32> color_pool;
-    draw::PointStyle const point_style({.color_index = color_pool.get_and_use_color(), .filled_shape = 10});
-    plot::cs::Point<csid::painter> point_painter{0.0, 0.0};
-    painter_coordinate_system.add_point(layer, point_style, point_painter);
+    draw::PointStyle const point0_style({.color_index = color_pool.get_and_use_color(), .filled_shape = 10});
+    draw::PointStyle const point1_style({.color_index = color_pool.get_and_use_color(), .filled_shape = 10});
+    draw::RectangleStyle const rectangle_style({.line_color = color::black, .line_width = 2.0});
 
-    window.register_draggable(painter_coordinate_system, &point_painter);
+    auto bounding_box_from = [](auto const& p0, auto const& p1) -> plot::cs::Rectangle<csid::painter>
+    {
+      double const left = std::min(p0.x(), p1.x());
+      double const right = std::max(p0.x(), p1.x());
+      double const top = std::min(p0.y(), p1.y());
+      double const bottom = std::max(p0.y(), p1.y());
+      return {left, top, right - left, bottom - top};
+    };
+
+    plot::cs::Point<csid::painter> point0_painter{-100.0, -50.0};
+    plot::cs::Point<csid::painter> point1_painter{100.0, 50.0};
+
+    plot::cs::Rectangle<csid::painter> rectangle_painter = bounding_box_from(point0_painter, point1_painter);
+    painter_coordinate_system.add_rectangle(layer, rectangle_style, rectangle_painter);
+    painter_coordinate_system.add_point(layer, point0_style, point0_painter);
+    painter_coordinate_system.add_point(layer, point1_style, point1_painter);
+
+    using PainterPoint = math::cs::Point<csid::painter>;
+    using DragRestriction = std::function<PainterPoint(PainterPoint const&)>;
+
+    window.register_draggable(painter_coordinate_system, &point0_painter,
+        DragRestriction{[&](PainterPoint const& new_position) -> PainterPoint
+        {
+          rectangle_painter = bounding_box_from(new_position, point1_painter);
+          painter_coordinate_system.add_rectangle(layer, rectangle_style, rectangle_painter);
+          return new_position;
+        }});
+
+    window.register_draggable(painter_coordinate_system, &point1_painter,
+        DragRestriction{[&](PainterPoint const& new_position) -> PainterPoint
+        {
+          rectangle_painter = bounding_box_from(point0_painter, new_position);
+          painter_coordinate_system.add_rectangle(layer, rectangle_style, rectangle_painter);
+          return new_position;
+        }});
 
     // Open window, handle event loop in a separate thread. This must be constructed after the draw stuff, so that it is destructed first!
     std::thread event_loop([&](){
